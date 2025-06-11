4862dda95c61fa5607fc350431372db36be58b2a
Ye-iii <jiye63892@eleve.isep.fr>
2025-06-11 14:48:11 +0200

    Create database.php

---
+++ database.php
@@ -0,0 +1,20 @@
+<?php
+$host = '144.76.54.100';
+$dbname = 'G2';
+$user = 'G2';
+$pass = 'APPG2-BDD';
+
+//$host = 'localhost';
+//$dbname = 'dBreeze';
+//$user = 'root';
+//$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
+//    $pdo = new PDO("mysql:host=$host;dbname=$dbname;port=3307;charset=utf8mb4", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie à la base de données.";
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
